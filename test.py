69ff732b49fe55be904332022be70467469acaf1
mayuchao <mayuchao@wisetv.com.cn>
2019-01-26 19:29:01 +0800

    需要安装的第三方库

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import pickle
 from luminoth import predict
 
 def __main():
+    # requested packeges:
+    # dm-sonnet, easydict, scikit-video, click
     with open( "param.pickle", "rb" ) as file:
         param = pickle.load( file )
 
